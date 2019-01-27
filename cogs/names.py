af531f54bd9f6b2c6624706b128b7982cb5e4b00
SourSpoon <SourSpoon@users.noreply.github.com>
2019-01-27 01:48:19 +0000

    centralised ids, added comments to convert

--- cogs/names.py
+++ cogs/names.py
@@ -7,17 +7,18 @@ from cogs.utils.convert import RSN
 class Names:
     def __init__(self, bot):
         self.bot = bot
-        self.db:SQL = bot.database
+        self.db: SQL = bot.database
+        self.ids = self.bot.ids
 
     @commands.command()
-    async def set_rsn(self, ctx, *, runescape_name:RSN):
+    async def set_rsn(self, ctx, *, runescape_name: RSN):  # type RSN is actually a str, see convert.py
         """
         Changes the name we have on file for you,
         RSNs must be between 3 and 12 characters containing only Alpha-Numeric characters and spaces
         """
         await self.db.add_user(ctx.author.id, runescape_name)
         await ctx.author.edit(nick=runescape_name)
-        unverified_role = discord.utils.get(ctx.guild.roles, id=536658901203025941)
+        unverified_role = discord.utils.get(ctx.guild.roles, id=self.ids['unknown_rsn'])
         if unverified_role in ctx.author.roles:
             await ctx.author.remove_roles(unverified_role)
 
@@ -31,7 +32,7 @@ class Names:
         """
         await self.db.add_user(target.id, runescape_name)
         await target.edit(nick=runescape_name)
-        unverified_role = discord.utils.get(ctx.guild.roles, id=536658901203025941)
+        unverified_role = discord.utils.get(ctx.guild.roles, id=self.ids['unknown_rsn'])
         await target.remove_roles(unverified_role)
         if unverified_role in target.roles:
             await target.remove_roles(unverified_role)
