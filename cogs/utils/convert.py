fc0cc4a20ebf0a1d6ef2e7eb7056001821d73971
SourSpoon <SourSpoon@users.noreply.github.com>
2019-05-02 00:30:21 +0100

    '-' is also an approved char, albeit rare

--- cogs/utils/convert.py
+++ cogs/utils/convert.py
@@ -8,7 +8,7 @@ from cogs.error import BadRunescapeName
 
 class RSN(commands.Converter):
     async def convert(self, ctx, argument):
-        runescape_name = re.match(r'^[a-zA-Z0-9 _]{2,12}$', argument)
+        runescape_name = re.match(r'^[a-zA-Z0-9 _-]{2,12}$', argument)
         if runescape_name is None:
             raise BadRunescapeName
         return runescape_name.string
